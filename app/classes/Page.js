a4408f82814426e3d0830f78449aa5730875ccf5
jtgomba <jgomba@hotmail.co.uk>
2023-11-03 01:23:47 +0900

    feat: added button animation

--- app/classes/Page.js
+++ app/classes/Page.js
@@ -1,8 +1,10 @@
 import TextReveal from '../animations/TextReveal';
+import Button from '../animations/Button';
 
 export default class Page {
   constructor() {
     this._createTextReveals();
+    this._createButtons();
   }
 
   _createTextReveals() {
@@ -15,6 +17,17 @@ export default class Page {
     textItems.forEach((text) => {
       new TextReveal({
         element: text,
+      });
+    });
+  }
+
+  _createButtons() {
+    const buttons = [...document.querySelectorAll('[data-animation="button"]')];
+    if (!buttons) return;
+
+    buttons.forEach((button) => {
+      new Button({
+        element: button,
       });
     });
   }
