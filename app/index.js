fa5e3a0f16b70504c50326515315672bde437a8e
jtgomba <jgomba@hotmail.co.uk>
2023-11-03 00:24:22 +0900

    feat: added smooth scrolling

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
 import '../styles/index.scss';
+import Lenis from '@studio-freight/lenis';
+
+class App {
+  constructor() {
+    this._createLenis();
+
+    this._render();
+  }
+
+  _createLenis() {
+    this.lenis = new Lenis({
+      lerp: 0.15,
+    });
+  }
+
+  _render(time) {
+    this.lenis.raf(time);
+
+    requestAnimationFrame(this._render.bind(this));
+  }
+}
+
+new App();
